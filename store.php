80a46f2824c0e656e979489b3497f26c9eed52ef
Yamato Nakagami <yamatonakagami@sss.local>
2017-10-06 13:34:51 +0900

    Initial Commit

---
+++ store.php
@@ -0,0 +1,13 @@
+<?php
+
+require('functions.php');
+
+$res = checkReferer();
+if($res != 'back'){
+  header('location: ./index.php');
+}elseif($res == 'index'){
+  header('location: ./index.php');
+}else{
+  header('location: '.$_SERVER['HTTP_REFERER'].'');
+}
+?>
